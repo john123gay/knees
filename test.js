ec5a6ee0ebdd307db520a1a689d0b3cea7fd4e3e
john <one23four5ate9@yahoo.com>
2019-02-09 17:49:00 -0500

    first test

---
+++ test.js
@@ -0,0 +1,14 @@
+$("#buzzbutton").on("click", function() {
+
+
+    var queryURL = "http://69.120.124.59:5050/buzz";
+    
+    $.ajax({
+         url: queryURL,
+         method: "GET",
+         crossOrigin: true
+       }).done(function(response) {
+           console.log(response);
+           console.log("buzz");
+       });    
+    });
